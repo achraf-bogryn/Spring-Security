881d22dea4eba78a1fb8207d31244c85efbbe61c
achraf-bogryn <achrafbogryn12345@gmial.com>
2024-05-14 00:32:25 +0100

    add new config to package configuration

---
+++ src/main/java/com/jwt/securitytutorial/filter/Config/CustomLogoutHandler.java
@@ -0,0 +1,37 @@
+package com.jwt.securitytutorial.filter.Config;
+
+import com.jwt.securitytutorial.model.Token;
+import com.jwt.securitytutorial.repository.TokenRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+
+public class CustomLogoutHandler implements LogoutHandler {
+    private final TokenRepository tokenRepository;
+
+    public CustomLogoutHandler(TokenRepository tokenRepository) {
+        this.tokenRepository = tokenRepository;
+    }
+
+    @Override
+    public void logout(HttpServletRequest request
+            , HttpServletResponse response
+            , Authentication authentication) {
+
+        String authHeader = request.getHeader("Authorization");
+
+        if(authHeader == null || !authHeader.startsWith("Bearer ")) {
+            return;
+        }
+
+        String token = authHeader.substring(7);
+        Token storedToken = tokenRepository.findByToken(token).
+                orElse(null);
+
+        if(storedToken != null) {
+            storedToken.setLoggedOut(true);
+            tokenRepository.save(storedToken);
+        }
+    }
+}
