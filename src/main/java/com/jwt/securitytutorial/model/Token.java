e14546146c36567f53feb256e15a86d0480ce17d
achraf-bogryn <achrafbogryn12345@gmial.com>
2024-05-14 00:33:03 +0100

    add new Entity

---
+++ src/main/java/com/jwt/securitytutorial/model/Token.java
@@ -0,0 +1,35 @@
+package com.jwt.securitytutorial.model;
+
+//public class Token {
+//}
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter @Setter
+@Entity
+@Table(name = "token")
+public class Token {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "token")
+    private String token;
+
+    @Column(name = "is_logged_out")
+    private boolean loggedOut;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+}
